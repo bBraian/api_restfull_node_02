f0e6c12b7a92678f6a410ba8987f315d29fe6303
bBraian <braianvoficial@gmail.com>
2023-03-01 22:52:01 -0300

    update: configuring migrations

--- src/database.js
+++ src/database.js
@@ -1,8 +1,14 @@
 import knex from "knex";
 
-export const setupKnex = knex({
+export const config = {
     client: 'sqlite',
     connection: {
-        filename: './tmp/app.db'
+        filename: './db/app.db'
+    },
+    useNullAsDefault: true,
+    migrations: {
+        directory: './db/migrations'
     }
-});
+}
+
+export const setupKnex = knex(config);
